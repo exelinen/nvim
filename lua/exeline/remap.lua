1f978969cea2f45690b558c8aaccd2a620547f8d
Ari Luoma <ari@ariluoma.net>
2023-08-27 11:22:27 +0300

    Telescope remap buffers, kommentteja

--- lua/exeline/remap.lua
+++ lua/exeline/remap.lua
@@ -12,12 +12,12 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("x", "<leader>p", "\"_dP")
 
-vim.keymap.set("n", "<leader>y", "\"+y")
-vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
+vim.keymap.set("n", "<leader>y", "\"+y") -- ??
+vim.keymap.set("v", "<leader>y", "\"+y") -- ??
+vim.keymap.set("n", "<leader>Y", "\"+Y") -- ??
 
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set("n", "<leader>d", "\"_d") -- ??
+vim.keymap.set("v", "<leader>d", "\"_d") -- ??
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
@@ -26,11 +26,11 @@ vim.keymap.set("n", "<leader>f", function()
     vim.lsp.buf.format()
 end)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz") -- ??
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz") -- ?? 
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz") -- ??
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz") -- ??
+-- Haetaan sanaa kursorin alta ja saadaan heti se vaihtaa johonkin toiseen
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 vim.keymap.set("n", "<leader><leader>", function()
